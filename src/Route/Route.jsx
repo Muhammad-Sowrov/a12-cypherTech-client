8028b6e807e93b6c371a77025e910d849a381b2a
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-11-29 14:52:10 +0600

    simple navbar added

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -3,18 +3,17 @@ import Error from "../Pages/Error/Error";
 import Home from "../Pages/Home/Home/Home";
 import Layout from "../Layout/Layout";
 
-
-const Route = createBrowserRouter ([
-    {
-        path: '/',
-        element: <Layout></Layout>,
-        errorElement:<Error></Error>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            }
-			]
-		}
-])
+const Route = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout></Layout>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
 export default Route;
