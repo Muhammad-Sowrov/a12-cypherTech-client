3fdb21d38739cddf5cd5b568535aebd25e1d5471
Muhammad-Sowrov <muhammadsowrov53@gmail.com>
2023-11-29 14:10:59 +0600

    initialize project with vite, tailwind and daisyUI

---
+++ src/Route/Route.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Error from "../Pages/Error/Error";
+import Home from "../Pages/Home/Home/Home";
+import Layout from "../Layout/Layout";
+
+
+const Route = createBrowserRouter ([
+    {
+        path: '/',
+        element: <Layout></Layout>,
+        errorElement:<Error></Error>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }
+			]
+		}
+])
+export default Route;
